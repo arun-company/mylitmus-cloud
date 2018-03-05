6df69945d81d75115cce5d6871cfc0a7235df5ec
arun-company <developer@arun.company>
2018-03-05 15:21:55 +0700

    [TASK] 404 Page

---
+++ layouts/404layout.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <no-ssr>
+      <Menu />
+    </no-ssr>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+  import Menu from '~/components/Menu.vue'
+
+  export default {
+    components: {
+      Menu
+    },
+    head: {
+      bodyAttrs: {
+        class: 'body'
+      }
+    }
+  }
+</script>
+<style>
+  #search-box {
+    border: none;
+    background: transparent;
+    text-align: right;
+    vertical-align: bottom;
+    padding-bottom: 0;
+    margin-bottom: 0;
+  }
+</style>
