38fc87c269ead0cedfd1f060d3e6f2f5a88cf5d7
arun-company <developer@arun.company>
2018-04-01 17:02:05 +0700

    [TASK] Zone Setting

--- src/pages/zone/Settings.vue
+++ src/pages/zone/Settings.vue
@@ -2,7 +2,15 @@
     <div class="section">
       <div class="div-block-3">
         <div class="div-block-4">
-          <div class="text-block-5">Home</div>
+          <router-link class="link" to="/">
+            Home
+          </router-link>
+        <div class="text-block-5">&gt;</div>
+          <router-link to="/site" class="link">
+            Default
+          </router-link>
+        <div class="text-block-5">&gt;</div>
+        <div v-if="zonename" class="text-block-5">{{zonename}}</div>
         </div>
         <div class="div-block-4 search">
           <input id="search-box" class="w-input" v-model="search" type="text" :placeholder="searchTitle"><a href="#" class="w-inline-block"><img src="public/images/search.333333.png" width="20" height="20" class="image"></a></div>
@@ -10,10 +18,26 @@
       <div class="w-container">
         <h1 class="heading">{{headerTitle}}</h1>
       </div>
-      
-      <div class="div-block-9">
-      
+      <div class="w-container">
+      <div class="reporting-all">
+        <div class="div-block-reporting" v-for="i in [1,2,3,4,5]">
+          <div class="div-block-10"><a href="zone.html" class="heading-4">Setting Sensor</a></div>
+          <div class="content">
+            <div class="div-block-12">
+              <template v-for="setting in settings">
+                <div  class="div-block-11">
+                  <div class="div-block-18">{{setting.sensorType.name + ' 상한'}} : {{setting.min_value}}</div>
+                </div>
+                <div class="div-block-11">
+                  <div class="div-block-18">{{setting.sensorType.name + ' 하한'}}: {{setting.max_value}}</div>
+                </div>
+              </template>
+            </div>
+          </div>
+        </div>
       </div>
+      </div>
+              
     </div>
 </template>
 
@@ -23,7 +47,8 @@
 
   import EventGraph from '@/components/charts/EventGraph'
   import TextCard from '@/components/dashboard/TextCard'
-  import ServiceStatusBar from '@/components/dashboard/ServiceStatusBar'
+  import ServiceStatusBar from '@/components/dashboard/ServiceStatusBar'    
+  import { API_BASE } from '@/global'
 
   export default {
     components: { EventGraph, TextCard, ServiceStatusBar },
@@ -35,25 +60,33 @@
           {id:5, name:'Settings', icon:'001-cogwheel.png', path:'/zone-settings', class:'w--current'},
         ]
       return {
+        zonename: localStorage.getItem('zonename'),
         search: '',
         open: this.drawer,
         headerTitle: 'Settings',
         searchTitle: 'Search sites ...',
         items: [
-          {id:1, name:"Default",description:""},
+          
         ],
+        settings:null
       }
     },
     computed:
     {
-      filteredItems:function()
-      {
-        var self=this;
-        return this.items.filter(function(item){return item.name.toLowerCase().indexOf(self.search.toLowerCase())>=0;});
-      }
+      
     },
     mounted () {
-      // this.$store.dispatch('setMenuItems', this.menuItems)
+      this.getSettings()
+    },
+    methods: {
+      getSettings () {
+        var zondId = localStorage.getItem('zoneid');
+        const SENSORTYPES_API = `${API_BASE}/v1/zones/${zondId}/sensorTypes`
+        axios.get(SENSORTYPES_API).then(res => {
+          this.settings = res.data
+        })
+      }
+
     }
   }
 </script>
